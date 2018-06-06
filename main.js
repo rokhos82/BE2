acad552d2981242b3d633dbd02ec409a26058f6b
Justin Lane <justin.l.lane@gmail.com>
2018-06-05 21:35:29 -0600

    Hello World

---
+++ main.js
@@ -0,0 +1,22 @@
+const {app,BrowserWindow,ipcMain} = require('electron');
+const Store = require('electron-store');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadURL(`file://${__dirname}/src/index.html`);
+}
+
+app.on('ready',createWindow);
+
+app.on('window-all-close',function() {
+  // Is the platform OS X?
+  if(process.platform !== "darwin") {
+    app.quit();
+  }
+});
